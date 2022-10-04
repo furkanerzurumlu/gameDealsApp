b7c133f5f8306879f6f522bf593f20678a9ca4fe
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-10-04 15:49:51 +0300

    first commit

---
+++ gameDeals/Screen/DealsCollectionViewCell/DealsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DealsCollectionViewCell.swift
+//  gameDeals
+//
+//  Created by Furkan Erzurumlu on 4.10.2022.
+//
+
+import UIKit
+
+class DealsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ gameDeals/Screen/Main/HomePageVM.swift
@@ -0,0 +1,8 @@
+//
+//  HomePageVM.swift
+//  gameDeals
+//
+//  Created by Furkan Erzurumlu on 4.10.2022.
+//
+
+import Foundation
